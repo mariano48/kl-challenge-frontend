3cad9b5fb626b6613e870ee74e5de2a145d3f070
mariano48 <marianosolis423@gmail.com>
2024-09-23 12:26:55 -0300

    Add movies mocks

---
+++ cypress/fixtures/movies.ts
@@ -0,0 +1,17 @@
+import { IMovie } from "../../src/types/Movies";
+
+export const initialMovies: IMovie[] = [
+  {
+    actor_1: "Danny Glover",
+    actor_2: "James Roday",
+    actor_3: "Robin Weigert",
+    director: "Tom Brown",
+    distributor: "TBD",
+    geolocation: { lat: 37.78465, lng: -122.40709 },
+    locations: "Church Street MUNI Station",
+    production_company: "Pushing Dead , LLC",
+    release_year: "2016",
+    title: "Pushing Dead",
+    writer: "Tom Brown",
+  },
+];
