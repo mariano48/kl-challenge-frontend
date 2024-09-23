14068eb3da9df25dbee6a6955ebe2812882824c9
mariano48 <marianosolis423@gmail.com>
2024-09-23 12:22:59 -0300

    Add GoogleMap component

---
+++ src/components/map/GoogleMap.tsx
@@ -0,0 +1,43 @@
+import { Map } from "@vis.gl/react-google-maps";
+import { IMovie } from "../../types/Movies";
+import CustomMarker from "./MarkerWithInfo";
+import { useState } from "react";
+import { SAN_FRANCISCO_CENTER } from "../../constants/constants";
+
+interface IMapProps {
+  movies?: IMovie[];
+}
+
+export default function GoogleMap({ movies }: IMapProps) {
+  const [openMovieView, setOpenMovieView] = useState<number | null>();
+
+  const handleMarkerClick = (id: number) => {
+    setOpenMovieView(openMovieView === id ? null : id);
+  };
+
+  return (
+    <div className="h-full w-full flex items-center justify-center text-black">
+      {movies && (
+        <Map
+          className="w-full h-full"
+          defaultCenter={SAN_FRANCISCO_CENTER}
+          defaultZoom={13}
+          mapTypeControl={false}
+          streetViewControl={false}
+        >
+          {movies.map((movie, i) => {
+            if (typeof movie.geolocation === "string") return null;
+            return (
+              <CustomMarker
+                key={i}
+                movie={movie}
+                isOpen={openMovieView === i}
+                onMarkerClick={() => handleMarkerClick(i)}
+              />
+            );
+          })}
+        </Map>
+      )}
+    </div>
+  );
+}
