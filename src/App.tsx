f1f9ccc9dee958ce8f1ab96f48c88bccce7ae59f
mariano48 <marianosolis423@gmail.com>
2024-09-23 12:25:37 -0300

    Change App.tsx render to include SearchBar and MapContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import MapContainer from "./components/map/MapContainer";
+import SearchBar from "./components/searchbar/SearchBar";
 import "./index.css";
 function App() {
-  return <p className="text-3xl text-red-500">Kinda Lab Challenge</p>;
+  return (
+    <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-start bg-white">
+      <SearchBar />
+      <MapContainer />
+    </div>
+  );
 }
 
 export default App;
